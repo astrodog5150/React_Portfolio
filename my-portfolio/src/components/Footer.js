e857534e4b056257a366fbf8e95a27fb46ad6cb5
Jeremy <jbjarvis@mac.com>
2024-03-15 07:45:33 -0500

    made simple components

---
+++ my-portfolio/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="https://github.com/astrodog5150" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+        {/* add any other accounts, if i ever make them */}
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
